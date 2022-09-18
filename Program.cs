697a82d541d8deff2f96ccbea548de40c00f907e
Nicholas Rodriguez <niickrodriguez@ou.edu>
2022-09-17 23:08:49 -0500

    hello

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronBarCode;
+
+namespace QRCodeGenerator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GeneratedBarcode Qrcode = IronBarCode.QRCodeWriter.CreateQrCode("https://www.facebook.com/wayne.mcpherson.7777");
+            Qrcode.SaveAsPng("QrCode.png");
+        }
+    }
+}
